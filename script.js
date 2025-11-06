64b7032339833ae3de92626bcb58bb4ee25406d9
ZetWu <63952346+zetwuu@users.noreply.github.com>
2025-11-06 13:41:53 +0000

    feat: Maîtrise des variables et conditions

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("Laboratoire prêt !");
+
+const nom = "Alexandre";
+let age = 16;
+//nom = "Didier" 
+//script.js:5 Uncaught TypeError: Assignment to constant variable. at script.js:5:5
+
+age = 18
+console.log(nom, age)
+
+const motDePasseAttendu = "secret123";
+let motDePasseUtilisateur = "secret123";
+
+if (motDePasseUtilisateur === motDePasseAttendu) {
+    console.log("Accès autorisé. Bienvenue !");
+} else if (motDePasseUtilisateur.length < 8 ) {
+    console.log("Accès refusé. Votre mot de passe est trop court (8 caractères min).");
+} else {
+    console.log("Accès refusé. Mot de passe incorrect.");
+}

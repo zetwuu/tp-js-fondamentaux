65d79c8681298ff63fdcb4252124bb6821778296
ZetWu <63952346+zetwuu@users.noreply.github.com>
2025-11-06 14:42:37 +0000

    feat: Maîtrise des Tableaux et Boucles for

--- script.js
+++ script.js
@@ -36,3 +36,35 @@ console.log("Je m'appelle", moi.prenom, moi.nom, ", j'ai", moi.age, "ans", ", j'
 moi.age = 19;
 moi.ville = "Lyon"
 console.log(moi)
+
+const notes = [12, 15, 9, 18];
+
+// Affiche 12
+console.log(notes[0]);
+ // Affiche 15
+console.log(notes[1]);
+// Affiche 9
+console.log(notes[2])
+// Affiche 18
+console.log(notes[3])
+// Affiche 4
+console.log("Le nombre de notes et de : ",notes.length);
+
+for (let i = 0; i < notes.length; i++) {
+    console.log("La note à l'indice " + i + " est " + notes[i]);
+}
+
+const classe = [
+    { prenom: 'Alice', age: 18 },
+    { prenom: 'Bob', age: 17 },
+];
+
+let nombreMajeurs = 0;
+
+for (let i = 0; i < classe.length; i++) {
+     if (classe[i].age >= 18) { 
+       nombreMajeurs++;
+       console.log("Le nombre d'élèves majeurs est de :", nombreMajeurs);
+       console.log(classe[i].prenom + " est majeur(e).");
+    }
+}
